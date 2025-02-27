4a60c361c19a6488ef8643a544de9fd4ec8154fe
Michael Kochis(michael.kochis) <michael.kochis@infosys.com>
2025-02-27 07:30:59 -0600

    Return to Person

--- window/gender_window.py
+++ window/gender_window.py
@@ -24,7 +24,7 @@ class GenderWindow:
     def getGenderList(self):
         returnThis = []
         response, summary, keys = self.db.driver.execute_query(
-            CypherBuilder().match_all_line("p", "Mask")
+            CypherBuilder().match_all_line("p", "Person")
             .where("p.gender IS NULL").limit(60)
             .return_line().text()
         )
@@ -64,12 +64,12 @@ class GenderWindow:
             for val in range(1, n):
                 new_name += ' ' + val;
             response, summary, keys = self.db.driver.execute_query(
-                CypherBuilder().match_all_line("m", "Mask")
+                CypherBuilder().match_all_line("m", "Person")
                 .where("m.name = \"" + new_name + "\"")
                 .node_property("m", "gender", neoGender)
                 .return_line().text()
             )
-            print("Mask " + new_name + " is now gender: " + neoGender)
+            print("Person " + new_name + " is now gender: " + neoGender)
             neolist = self.getGenderList()
             self.window["gender_list"].Update(neolist)
         else:
